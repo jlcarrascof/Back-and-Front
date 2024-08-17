3b3ab8b6757f8833f2f7f40701f78ee8f9a0b652
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-16 20:03:30 -0400

    chore: Add backend server configuration and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load env variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to manage JSON data and request logging
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error to connecto to MongoDB', err));
